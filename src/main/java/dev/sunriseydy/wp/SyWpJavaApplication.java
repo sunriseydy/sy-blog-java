3b7f70f807586f8a99e9f8b3868f1c9e85b744e0
SunriseYDY <me@sunriseydy.dev>
2022-03-01 15:40:02 +0800

    init

---
+++ src/main/java/dev/sunriseydy/wp/SyWpJavaApplication.java
@@ -0,0 +1,13 @@
+package dev.sunriseydy.wp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyWpJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyWpJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/sunriseydy/wp/SyWpJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.sunriseydy.wp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyWpJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
