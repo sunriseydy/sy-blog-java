803294d825f0e0d40ca9672a19d1c697afaa9909
SunriseYDY <me@sunriseydy.dev>
2022-03-29 14:36:00 +0800

    [ADD]将有BasicAuth注解的controller方法添加swagger登陆认证

--- src/main/java/dev/sunriseydy/wp/common/annotion/BasicAuth.java
+++ src/main/java/dev/sunriseydy/wp/common/annotion/BasicAuth.java
@@ -1,5 +1,8 @@
 package dev.sunriseydy.wp.common.annotion;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -11,6 +14,7 @@ import java.lang.annotation.Target;
  */
 @Target(ElementType.METHOD)
 @Retention(RetentionPolicy.RUNTIME)
+@Operation(security = { @SecurityRequirement(name = "basicScheme") })
 public @interface BasicAuth {
     String value() default "";
 }
---
+++ src/main/java/dev/sunriseydy/wp/common/config/SpringDocConfig.java
@@ -0,0 +1,26 @@
+package dev.sunriseydy.wp.common.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author SunriseYDY
+ * @date 2022-03-29 13:08
+ */
+@Configuration(proxyBeanMethods = false)
+public class SpringDocConfig {
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+        return new OpenAPI()
+                .components(new Components()
+                    .addSecuritySchemes("basicScheme", new SecurityScheme()
+                        .type(SecurityScheme.Type.HTTP).scheme("basic")))
+                .info(new Info().title("SY-WP-JAVA API")
+                .version(appVersion));
+    }
+}
