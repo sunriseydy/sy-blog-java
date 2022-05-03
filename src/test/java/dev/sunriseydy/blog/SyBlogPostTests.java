0494e54e08de40ce8153e1769ac103e41bce4aa7
SunriseYDY <me@sunriseydy.dev>
2022-05-03 12:48:03 +0800

    [ADD]添加将markdown的文章存入数据库的测试类

--- src/test/java/dev/sunriseydy/blog/SyBlogPostTests.java
+++ src/test/java/dev/sunriseydy/blog/SyBlogPostTests.java
@@ -4,12 +4,17 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import dev.sunriseydy.blog.db.typecho.entity.TypechoContents;
 import dev.sunriseydy.blog.db.typecho.mapper.TypechoContentsMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.util.StreamUtils;
 
 import javax.annotation.Resource;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileWriter;
 import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 /**
@@ -41,6 +46,20 @@ public class SyBlogPostTests {
             writer.close();
             contentsMapper.update(null, new UpdateWrapper<TypechoContents>().lambda().eq(TypechoContents::getCid, object.getCid())
                     .set(TypechoContents::getText, object.getText()));
+        }
+    }
+
+    @Test
+    public void readPostMarkDown() throws Exception {
+        File dir = new File("/home/sunriseydy/Program/project/sy-blog/blog/markdown/");
+        File[] files = dir.listFiles();
+        for (File file : files) {
+            String id = StringUtils.substringBefore(file.getName(), "#");
+            System.out.println(id);
+            FileInputStream reader = new FileInputStream(file);
+            String data = StreamUtils.copyToString(reader, StandardCharsets.UTF_8);
+            reader.close();
+            // contentsMapper.updateById(TypechoContents.builder().cid(Integer.valueOf(id)).text("<!--markdown-->" + data).build());
         }
     }
 }
