f6fb55ce2f56e668659b0bdbebcda981ae5ee146
SunriseYDY <me@sunriseydy.dev>
2022-05-14 18:02:52 +0800

    [IMP]修改启动脚本

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 set -o errexit # 只要发生错误，就终止执行
 set -o pipefail # 只要一个子命令失败，整个管道命令就失败，脚本就会终止执行
 
+export SPRING_PROFILES_ACTIVE=default
 source .local.env
 
 git checkout master
@@ -10,4 +11,4 @@ git reset --hard origin/master
 
 mvn -DskipTests=true clean package
 
-nohup java -jar target/sy-blog-java-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+nohup java -jar -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE target/sy-blog-java-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
