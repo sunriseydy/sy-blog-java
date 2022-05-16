29670dcb22676d67d1979813f1a7a7bb6e71f563
SunriseYDY <me@sunriseydy.dev>
2022-05-16 15:00:32 +0800

    [IMP]修改启动脚本

--- run.sh
+++ run.sh
@@ -6,11 +6,13 @@ export SPRING_PROFILES_ACTIVE=default
 source .env.local
 
 if [ "$1" != "-l" ]; then
-  git checkout master
-  git fetch
-  git reset --hard origin/master
+  git pull
 
   mvn -DskipTests=true clean package
 fi
 
-nohup java -jar -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE target/sy-blog-java-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+if [ "$SPRING_PROFILES_ACTIVE" == "default" ]; then
+  java -jar -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE target/sy-blog-java-0.0.1-SNAPSHOT.jar
+else
+  nohup java -jar -Dspring.profiles.active=$SPRING_PROFILES_ACTIVE target/sy-blog-java-0.0.1-SNAPSHOT.jar > /dev/null 2>&1 &
+fi
